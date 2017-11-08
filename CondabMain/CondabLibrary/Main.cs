29418b393debbbf0b638bf1c2d335d2786693daa
hiteshbhatia <hiteshbhatia3559@gmail.com>
2017-11-08 06:19:57 +0530

    Testing of modules done, PoC done, integration pending

--- CondabMain/CondabLibrary/Main.cs
+++ CondabMain/CondabLibrary/Main.cs
@@ -12,10 +12,20 @@ namespace CondabLibrary
         public static void Main(String[] args)
         {
 
-            string genesisTransactions = "Hitesh sends Nikita 10 CDAaaa";
+            string genesisTransactions = "Hitesh sends Nikita 10 CDA";
             Block genesisBlock = new Block(PreviousHash:"0", transactions: genesisTransactions);
+            Console.WriteLine("Genesis blockhash is:");
             Console.WriteLine(genesisBlock.getBlockHash());
+
+            string block2transactions = "Nikita sends Hitesh 10 CDA";
+            Block block2 = new Block(genesisBlock.getBlockHash(), block2transactions);
+            Console.WriteLine("Block#2 blockhash is:");
+            Console.WriteLine(block2.getBlockHash());
             
+            string block3transactions = "Nikita sends her friend 10 CDA";
+            Block block3 = new Block(block2.getBlockHash(), block3transactions);
+            Console.WriteLine("Block#3 blockhash is:");
+            Console.WriteLine(block3.getBlockHash());
             Console.ReadLine();
         }
     }
